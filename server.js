87edfa2f85b3da6f07b35fc9ac9c2184d7f55fd0
Brooke Lynn <brookelynnny2015@gmail.com>
2018-11-12 18:53:50 -0500

    actual

---
+++ server.js
@@ -0,0 +1,10 @@
+// Starts the web server, imports app.js
+require('dotenv').config({silent: true});
+
+var server = require('./app');
+var port = process.env.PORT || 3000;
+
+server.listen(port, function() {
+  // eslint-disable-next-line no-console
+  console.log('Server running on port: %d', port);
+});
