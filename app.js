44799e48c5a2a565b712954572144442522adc0c
Haley Little <hpl15@my.fsu.edu>
2018-11-18 16:31:18 -0500

    IT WORKS!
    I added the partials and dashboard-pane folders to the views folder. I also switched the app.get statement in app.js to now render the index.ejs file (what michael made last time we met)

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// Load the home page
+app.set('view engine', 'ejs')
+app.get('/', function (req, res) {
+  res.render('index')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
